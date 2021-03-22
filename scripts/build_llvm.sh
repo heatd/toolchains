cc3d054f3c261048ba8026ee78d9ca008490f4ba
heatd <pedro.falcato@gmail.com>
2021-03-22 02:59:06 +0000

    llvm: Fix patch and add build script.
    Signed-off-by: heatd <pedro.falcato@gmail.com>

---
+++ scripts/build_llvm.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+if [ "$#" -ne "2" ]; then
+	echo "Bad usage: build_llvm.sh [staging_directory] [target_directory]"
+	exit 1
+fi
+
+LLVM_VER="11.0.0"
+
+ONYX_SRCDIR=$PWD
+
+staging_dir=$(realpath $1)
+target_dir=$(realpath $2)
+
+rm -rf $staging_dir
+
+toolchains/download_patch_llvm.sh $staging_dir
+
+cd $staging_dir
+
+LLVM_SRCDIR=$PWD/llvm-project-${LLVM_VER}
+
+mkdir build
+cd build
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_LINK_LLVM_DYLIB=ON \
+-DCLANG_LINK_CLANG_DYLIB=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_ENABLE_LTO=OFF \
+-DCMAKE_C_COMPILER=clang \
+-DCMAKE_CXX_COMPILER=clang++ \
+-DCMAKE_ASM_COMPILER=clang \
+-DLINUX_x86_64-unknown-linux-gnu_SYSROOT=/ \
+-DONYX_SRCDIR=$ONYX_SRCDIR \
+-DCMAKE_INSTALL_PREFIX= \
+-DCMAKE_MODULE_PATH=${ONYX_SRCDIR}/toolchains/cmake \
+-C ${LLVM_SRCDIR}/clang/cmake/caches/Onyx-stage2.cmake ${LLVM_SRCDIR}/llvm
+
+ninja
+DESTDIR=$target_dir ninja install

c033753912af76ed8f774dd44584d1b77fe24de6
Pedro Falcato <pedro.falcato@gmail.com>
2021-11-16 02:15:28 +0000

    scripts: Add copyright headers to the scripts
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- download_and_patch_tools.sh
+++ download_and_patch_tools.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# Copyright (c) 2021 Pedro Falcato
+# This file is part of Onyx, and is released under the terms of the MIT License
+# check LICENSE at the root directory for more information
+#
+# SPDX-License-Identifier: MIT
+#
+
 set -e
 
 if [ "$#" -ne "1" ]; then
